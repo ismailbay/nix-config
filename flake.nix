f6d695347a54d061f3dbd65a53bfb307cd61d0cd
Ismail Bay <ibay@Ismails-Mini.ibay.dev>
2023-11-30 12:12:57 +0100

    initial setup of mac mini

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "ismailbay's nix-config";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.11-darwin";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, 
+              nixpkgs, 
+              home-manager,
+              nix-darwin,
+              ...}: {
+
+    darwinConfigurations.melamar = nix-darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      modules = [ ./machines/melamar/default.nix ];
+    };
+
+  };
+}
---
+++ machines/melamar/default.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }:
+{
+
+  services.nix-daemon.enable = true;
+  
+  programs.zsh.enable = true;
+
+  homebrew = {
+    onActivation = {
+      autoUpdate = true;
+      upgrade = true;
+      cleanup = "zap";
+    };
+    enable = true;
+    
+    casks = [
+      "rectangle"
+    ];
+  };
+}
