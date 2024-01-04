543b5ae71abe59c430c2f108a71934bd4a31ca23
Ismail Bay <ismailbay@gmail.com>
2024-01-04 18:14:10 +0100

    feat: add k9s to home-manager

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -271,22 +271,22 @@ in {
     '';
   };
 
-  # k9s = {
-  #   enable = true;
-  #   plugin = {
-  #     #--- Create debug container for selected pod in current namespace
-  #     debug = {
-  #       shortCut = "Shift-D";
-  #       description = "Add debug container";
-  #       scopes = ["containers"];
-  #       command = "bash";
-  #       background = false;
-  #       confirm = true;
-  #       args = [
-  #         "-c"
-  #         "kubectl debug -it -n=$NAMESPACE $POD --target=$NAME --image=nicolaka/netshoot:v0.11 --share-processes -- bash"
-  #       ];
-  #     };
-  #   };
-  # };
+  k9s = {
+    enable = true;
+    plugin = {
+      #--- Create debug container for selected pod in current namespace
+      debug = {
+        shortCut = "Shift-D";
+        description = "Add debug container";
+        scopes = ["containers"];
+        command = "bash";
+        background = false;
+        confirm = true;
+        args = [
+          "-c"
+          "kubectl debug -it -n=$NAMESPACE $POD --target=$NAME --image=nicolaka/netshoot:v0.11 --share-processes -- bash"
+        ];
+      };
+    };
+  };
 }
