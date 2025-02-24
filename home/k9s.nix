50e532e150e245617f3b1569fb22bc2f98313679
Ismail Bay <ismailbay@gmail.com>
2025-02-24 19:46:21 +0100

    feat: add k9s plugins for swissarmyknife pod, ephemeral debug container

--- home/k9s.nix
+++ home/k9s.nix
@@ -3,9 +3,9 @@
     enable = true;
     plugin = {
       plugins = {
-        #--- Create swissarmy debug pod in current namespace
-        debug = {
-          shortCut = "Shift-D";
+        #--- create swissarmy debug pod in current namespace
+        swissarmy = {
+          shortCut = "Shift-K";
           description = "Add debug pod";
           scopes = ["pods"];
           command = "bash";
@@ -13,8 +13,49 @@
           confirm = true;
           args = [
             "-c"
-            "kubectl run -it -n=$NAMESPACE swissarmy --image=leodotcloud/swiss-army-knife:latest -- bash"
+            "kubectl run --kubeconfig=$KUBECONFI swissarmyknife -it --context $CONTEXT -n=$NAMESPACE --image=leodotcloud/swiss-army-knife:latest -- bash"
           ];
+        };
+        #--- create ephemeral debug container in current pod
+        debug = {
+          shortCut = "Shift-L";
+          description = "Add debug container";
+          dangerous = true;
+          scopes = ["containers"];
+          command = "bash";
+          background = false;
+          confirm = true;
+          args = [
+            "-c"
+            "kubectl --kubeconfig=$KUBECONFIG debug -it --context $CONTEXT -n=$NAMESPACE $POD --target=$NAME --image=nicolaka/netshoot:v0.13 --share-processes -- bash"
+          ];
+        };
+        watch-events = {
+          shortCut = "Shift-E";
+          confirm = false;
+          description = "Get Events";
+          scopes = ["all"];
+          command = "sh";
+          background = false;
+          args = [
+            "-c"
+            "kubectl events --context $CONTEXT --namespace $NAMESPACE --for $RESOURCE_NAME.$RESOURCE_GROUP/$NAME --watch"
+          ];
+        };
+        argocd = {
+          shortCut = "Shift-A";
+          description = "Sync ArgoCD Application";
+          scopes = ["application"];
+          command = "argocd";
+          args = [
+            "app"
+            "sync"
+            "$NAME"
+            "--app-namespace"
+            "$NAMESPACE"
+          ];
+          background = true;
+          confirm = true;
         };
       };
     };
